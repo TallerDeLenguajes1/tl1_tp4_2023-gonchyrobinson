7b5303af0fbe2634ebf35e13fbe4bd9a47bf2801
Gonzalo Robinson <gonchyrobinson@gmail.com>
2023-04-28 16:52:12 -0300

    Subo el tp4 completo

--- tp4P3.c
+++ tp4P3.c
@@ -25,23 +25,32 @@ void InsertarNodo(Lista* cabecera, Lista nodo);
 void MuestraLista(Lista lista);
 void MuestraEstructura(Tarea estruct);
 bool EsListaVacia(Lista lista);
-void RealizoTarea(Lista* pendientes, Lista* realizadas, Lista* pendientesNueva);
+void RealizoTarea(Lista* pendientes, Lista* realizadas, Lista* pendientesNueva, Lista* enProcesp);
 void CambiaTareas(Lista* pendientes, Lista* realizadas, Lista anterior);
 void CambiarTareaPendientes(Lista* pendientes);
 bool EsPrimerElemento(Lista lista);
 void LiberaMemoria(Lista realizadas, Lista pendientes);
 Lista DesenlazarNodoDeLaLista(Lista* cabecera);
-void menu(Lista* pendientes, Lista* realizadas, Lista* pendientesNueva);
+void menu(Lista* pendientes, Lista* realizadas, Lista* pendientesNueva, Lista* enProceso);
 Tarea BuscarTareaPorId(Lista pendientes, Lista realizadas, Lista pendientesNueva);
 Tarea BuscarTareaPorPalabraClave(Lista pendientes, Lista realizadas, Lista pendientesNueva);
+void EliminarTarea(Lista* tarea);
+void MostrarDatos(Lista tarea);
+void MoverDatos(Lista*pendientes,Lista*realizadas, Lista*EnProceso);
+Lista SeleccionaNodo(Lista lista, int id);
+void MoverTarea(Lista*pendientes, Lista*realizadas, Lista*enProceso, Lista nodoSacado);
+Lista DescenlazarNodoId(Lista* lista,int id);
+bool PerteneceTareaALaLista(Lista lista, int id);
 
 int main(){
     Lista pendientes=CrearListaVacia();
     Lista realizadas=CrearListaVacia();
     Lista pendientesNueva=CrearListaVacia();
+    Lista tareasEnProceso=CrearListaVacia();
     Lista aux;
-    menu(&pendientes,&realizadas,&pendientesNueva);
+    menu(&pendientes,&realizadas,&pendientesNueva,&tareasEnProceso);
     LiberaMemoria(realizadas,pendientesNueva);
+
 }
 
 Lista CrearListaVacia(){
@@ -66,9 +75,8 @@ Tarea CargaTarea(int i){
     return(aux);
 }
 void InsertarNodo(Lista* cabecera,Lista nodo){
-    Lista aux=nodo;
-    aux->siguiente=*cabecera;
-    *cabecera=aux;
+    nodo->siguiente=*cabecera;
+    *cabecera=nodo;
 }
 void MuestraEstructura(Tarea estruct){
     printf("\n.ID:  %d", estruct.TareaId);
@@ -99,7 +107,7 @@ Lista DesenlazarNodoDeLaLista(Lista* cabecera){
     return(aux);
 }
 
-void RealizoTarea(Lista* pendientes, Lista* realizadas, Lista* pendientesNueva){
+void RealizoTarea(Lista* pendientes, Lista* realizadas, Lista* pendientesNueva, Lista* enProceso){
     int realizado;
     Lista nodo;
     Lista anterior=NULL;
@@ -107,14 +115,22 @@ void RealizoTarea(Lista* pendientes, Lista* realizadas, Lista* pendientesNueva){
     {
         printf("\n********************************************\n"); 
         MuestraEstructura((*pendientes)->dato);
-        printf("\nRealizo la tarea: \n1- Si\n0-No\n");
+        printf("\nRealizo la tarea: \n1- Si\n0-No\n2-En proceso\n");
         scanf("%d", &realizado);
         nodo=DesenlazarNodoDeLaLista(pendientes);
-        if (realizado==1)
+        switch (realizado)
         {
+        case 0:
+            InsertarNodo(pendientesNueva,nodo);
+            break;
+        case 1:
             InsertarNodo(realizadas,nodo);
-        }else{
-            InsertarNodo(pendientesNueva,nodo);      
+            break;
+        case 2:
+            InsertarNodo(enProceso,nodo);
+        break;
+        default:
+            break;
         }
     } 
 }
@@ -148,15 +164,16 @@ void LiberaMemoria(Lista realizadas, Lista pendientes){
     
 }
 
-void menu(Lista* pendientes, Lista*realizadas, Lista*pendientesNueva){
+void menu(Lista* pendientes, Lista*realizadas, Lista*pendientesNueva, Lista* enProceso){
     int opcion;
     int bandera=0;
+    int tareaEliminar;
     Tarea tareaAuxiliar;
     printf("\n::::::::::::::::::::::MENU:::::::::::::::::::::::::\n");
     while (bandera==0)
     {
         printf("\n-----------------------------------------------------------\n");
-        printf("Ingrese una opcion:  \n0-salir\n1-Cargar Tareas\n2-Marcar tareas realizadas\n3- Mostrar Tareas Realizadas\n4-Buscar tareas por id\n5-Buscar tareas por palabra clave");
+        printf("Ingrese una opcion:  \n0-salir\n1-Cargar Tareas\n2-Marcar tareas realizadas\n3- Mostrar Tareas Realizadas\n4-Buscar tareas por id\n5-Buscar tareas por palabra clave\n6-Eliminar Tara\n7-MOstrar datos de la lista\n8-MOver Datos");
         printf("\nOpcion eleginda:  ");
         scanf("%d", &opcion);
         switch (opcion)
@@ -176,13 +193,15 @@ void menu(Lista* pendientes, Lista*realizadas, Lista*pendientesNueva){
             }
         break;
         case 2:
-            RealizoTarea(pendientes,realizadas,pendientesNueva);
+            RealizoTarea(pendientes,realizadas,pendientesNueva,enProceso);
         default:
         case 3:
             printf("\n;;;;;;;;;;;;;;;;;;;;TAREAS REALIZADAS;;;;;;;;;;;;;;;;;;;\n");
             MuestraLista(*realizadas);
             printf("\n++++++++++++++++++++++TAREAS PENDIENTES +++++++++++++++++++++++\n");
             MuestraLista(*pendientesNueva);
+            printf("\n++++++++++++++++++++++TAREAS EN PROCESO +++++++++++++++++++++++\n");
+            MuestraLista(*enProceso);
         break;
         case 4: 
             tareaAuxiliar=BuscarTareaPorId(*pendientes,*realizadas,*pendientesNueva);
@@ -199,9 +218,48 @@ void menu(Lista* pendientes, Lista*realizadas, Lista*pendientesNueva){
                 printf("\nNo se encontro la tarea buscada\n");
             }
         break;
+        case 6:
+            printf("\n.........................ELIMINA TAREA...................\n");
+            printf("\n-Ingrese de que lista desea eliminar una tarea:  \n1- tareas Pendientes\n2- tareas Realizadas\n3- tareas en proceso");
+            scanf("%d", &tareaEliminar);
+            switch (tareaEliminar)
+            {
+            case 1:
+                EliminarTarea(pendientesNueva);
+                break;
+            case 2:
+                EliminarTarea(realizadas);
+                break;
+            case 3:
+                EliminarTarea(enProceso);
+            break;
+            }
             
-        }
+        
+        break;
+        case 7:
+            printf("\n.........................Mostrar Datos...................\n");
+            printf("\n-Ingrese de que lista desea ver los datos:  \n1- tareas Pendientes\n2- tareas Realizadas\n3-En proceso");
+            scanf("%d", &tareaEliminar);
+            switch (tareaEliminar)
+            {
+            case 1:
+                MostrarDatos(*pendientesNueva);
+                break;
+            case 2:
+                MostrarDatos(*realizadas);
+            case 3:
+                MostrarDatos(*enProceso);
+            break;
+            default:
+                break;
+            }
+        break;
+        case 8:
+            printf("\n|||||||||||||||||||||||MOVER DATOS|||||||||||||||||||||||||||||||\n");
+            MoverDatos(pendientesNueva,realizadas,enProceso);
 
+    }
     }
     
 }
@@ -279,5 +337,179 @@ Tarea BuscarTareaPorPalabraClave(Lista pendientes, Lista realizadas, Lista pendi
         tareaBuscada=CargaTarea(INDEFINIDO);
     }
     return(tareaBuscada);
+    
+}
+void EliminarTarea(Lista* tarea){
+    if(!EsListaVacia(*tarea)){
+        Lista aux;
+        aux=(*tarea);
+        (*tarea)=(*tarea)->siguiente;
+        free(aux);
+    }else
+    {
+        printf("\n La lista esta vacia, por lo tanto, no hay elementos para eliminar");
+    }
+    
+}
+void MostrarDatos(Lista tarea){
+    int cant=0;
+    int tiempo=0;
+    while (!EsListaVacia(tarea))
+    {
+        cant+=1;  
+        tiempo+=tarea->dato.Duracion;
+        tarea=tarea->siguiente;
+    }
+    printf("\n{{{{{{{{{{{{{{{{{{{MUESTRA CANTIDAD DE TAREAS Y DURACION}}}}}}}}}}}}}}}}}}}");
+    printf("\nCantidad de tareas:  %d", cant);
+    printf("\n-Duracion total:  %d",tiempo);
+
+}
+void MoverDatos(Lista*pendientes,Lista*realizadas, Lista*EnProceso){
+            printf("\n;;;;;;;;;;;;;;;;;;;;TAREAS REALIZADAS;;;;;;;;;;;;;;;;;;;\n");
+            MuestraLista(*realizadas);
+            printf("\n++++++++++++++++++++++TAREAS PENDIENTES +++++++++++++++++++++++\n");
+            MuestraLista(*pendientes);
+            printf("\n++++++++++++++++++++++TAREAS EN PROCESO +++++++++++++++++++++++\n");
+            MuestraLista(*EnProceso);
+            printf("\nIngrese el id de la tarea que necesita modificar:   ");
+            int id;
+            int lista;
+            scanf("%d", &id);
+           Lista nodoSacado=CrearListaVacia();//recibe a donde apunta el nodo seleccionado
+           Lista nodoSeparado=CrearListaVacia();
+            if(PerteneceTareaALaLista(*pendientes,id)){
+                nodoSeparado=DescenlazarNodoId(pendientes,id);
+                lista=1;
+            }else
+            {
+                if (PerteneceTareaALaLista(*realizadas,id))
+                {
+                    nodoSeparado=DescenlazarNodoId(realizadas,id);
+                    lista=2;
+                }else
+                {
+                    if (PerteneceTareaALaLista(*EnProceso,id))
+                    {
+                        nodoSeparado=DescenlazarNodoId(EnProceso,id);
+                        lista=3;
+                    }else
+                    {
+                        nodoSeparado=NULL;
+                        printf("\nEl id seleccionado no existe");
+                    }
+                    
+                    
+                }
+                
+                
+            }
+            int tarea;
+            if (nodoSeparado)
+            {
+                printf("\n Ingrese que desea hacer con la tarea seleccionada:   \n1-Mover\n2-Eliminar la tarea\n3-No hacer nada  ");
+                scanf("%d", &tarea);
+                switch (tarea)
+                {
+                case 1:
+                    MoverTarea(pendientes,realizadas,EnProceso,nodoSeparado);
+                    break;
+                case 2:
+                    free(nodoSeparado);
+                    break;
+                case 3:
+                    switch (lista)
+                    {
+                    case 1:
+                        InsertarNodo(pendientes,nodoSeparado);
+                        break;
+                    case 2:
+                        InsertarNodo(realizadas,nodoSeparado);
+                    case 3:
+                        InsertarNodo(EnProceso,nodoSeparado);
+                    default:
+                        break;
+                    }
+                break;
+                }
+                }else
+                {
+                    printf("\nERROR al seleccionar tarea");
+                }
+                
+            
+}
+Lista SeleccionaNodo(Lista lista, int id){
+    Lista seleccionado=CrearListaVacia();
+    if (!EsListaVacia(lista))
+    {
+        while (!EsListaVacia(lista) && (lista)->dato.TareaId!=id)
+        {
+            lista=(lista)->siguiente;
+        }
+        if(!EsListaVacia(lista)){
+            seleccionado=lista;
+        }
+    }
+    return(seleccionado);
+    
+}
+
+void MoverTarea(Lista*pendientes, Lista*realizadas, Lista*enProceso, Lista nodoSacado){
+            int lista;
+            printf("\nIngrese a que lista desea insertar la tarea:  \n1-pendientes\n2-realizados\n3-en Proceso");
+            scanf("%d", &lista);
+            switch (lista)
+            {
+            case 1:
+                InsertarNodo(pendientes,nodoSacado);
+                break;
+            case 2:
+                InsertarNodo(realizadas,nodoSacado);
+                break;
+            case 3:
+                InsertarNodo(enProceso,nodoSacado);
+                break;
+            default:
+                break;
+            }
+}
+
+Lista DescenlazarNodoId(Lista* lista,int id){
+    Lista actual=*lista;
+    Lista anterior=*lista;
+    Lista seleccionado=NULL;
+    if (!EsListaVacia(*lista)){
+        if((*lista)->dato.TareaId==id)
+        {
+            seleccionado=*lista;
+            *lista=(*lista)->siguiente;
+        }else
+        {     
+            while (!EsListaVacia(actual) && (actual)->dato.TareaId!=id)
+            {   
+                anterior=*lista;
+                actual=actual->siguiente;
+            }
+            if(!EsListaVacia(actual)){
+                anterior->siguiente=actual->siguiente;
+                seleccionado=actual;
+            }
+        }
+    }
+    
+    return(seleccionado);
+    
+}
+bool PerteneceTareaALaLista(Lista lista, int id){
+    bool pertenece=false;
+    while (!EsListaVacia(lista) && lista->dato.TareaId!=id)
+    {
+        lista=lista->siguiente;
+    }
+    if(!EsListaVacia(lista)){
+        pertenece=true;
+    }
+    return(pertenece);
     
 }
